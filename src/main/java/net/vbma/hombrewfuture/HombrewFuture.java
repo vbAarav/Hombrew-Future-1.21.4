12e9810788b6d3e04e2023adfdf4b8a26d31aaae
vbAarav <37614889+vbAarav@users.noreply.github.com>
2024-12-23 11:10:30 +1100

    Imported and Setup Neo Forge Template

--- src/main/java/net/vbma/hombrewfuture/HombrewFuture.java
+++ src/main/java/net/vbma/hombrewfuture/HombrewFuture.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.vbma.hombrewfuture;
 
 import org.slf4j.Logger;
 
@@ -35,13 +35,13 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
-{
-    // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+@Mod(HombrewFuture.MODID)
+public class HombrewFuture
+    {
+
+    public static final String MODID = "hombrewfuture"; // Define mod id in a common place for everything to reference
+    private static final Logger LOGGER = LogUtils.getLogger(); // Directly reference a slf4j logger
+
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public HombrewFuture(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
